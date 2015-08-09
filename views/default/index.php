1ea31925d0eb1453921f718475e144de48e13ea9
pistol88 <pistol88@gmail.com>
2015-08-09 17:07:02 +0500

    Fix cart page error

--- views/default/index.php
+++ views/default/index.php
@@ -6,5 +6,5 @@ $this->title = Yii::t('cart', 'Cart');
 
 <div class="cart">
 	<h1><?=\Yii::t('cart', 'Cart');?></h1>
-	<?=ElementsList::widget(['template' => $this]); ?>
+	<?=ElementsList::widget(); ?>
 </div>
