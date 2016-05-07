7d1d11d5958ef0ca567c6091f3587bc86c663540
= <=>
2016-05-07 20:17:01 +0500

    formatting

--- Cart.php
+++ Cart.php
@@ -1,13 +1,16 @@
 <?php
 namespace pistol88\cart;
-use Yii;
+
+use yii;
 use yii\base\Component;
 use pistol88\cart\models\Cart as CartModel;
 use pistol88\cart\events\Cart as CartEvent;
 use pistol88\cart\models\CartElement;
 use pistol88\cart\events\CartElement as CartElementEvent;
 
-class Cart extends Component {
+class Cart extends Component
+{
+
     const EVENT_CART_INIT = 'cart_init';
     const EVENT_CART_TRUNCATE = 'cart_truncate';
     const EVENT_CART_COST = 'cart_cost';
@@ -24,11 +27,13 @@ class Cart extends Component {
     public $priceFormat = [2, '.', ''];
     public $id = null;
 
-    public function behaviors() {
+    public function behaviors()
+    {
         return $this->behaviors;
-	}
+    }
     
-    public function init() {
+    public function init()
+    {
         if($this->_cart == NULL) {
             $this->_cart = CartModel::find()->my();
             $this->id = $this->_cart->id;
@@ -38,7 +43,8 @@ class Cart extends Component {
         return $this;
     }
     
-    public function getCost() {
+    public function getCost()
+    {
         $cost = $this->_cart->getElements()->sum('price*count');
 
         $cartEvent = new CartEvent(['cart' => $this->_cart, 'cost' => $cost]);
@@ -52,15 +58,18 @@ class Cart extends Component {
         return $this->_cost;
     }
 
-    public function setCost($cost) {
+    public function setCost($cost)
+    {
         $this->_cost = $cost;
     }
 
-    public function getCart() {
+    public function getCart()
+    {
         return $this->_cart;
     }
 
-    public function getCostFormatted() {
+    public function getCostFormatted()
+    {
         $price = number_format($this->getCost(), $this->priceFormat[0], $this->priceFormat[1], $this->priceFormat[2]);
 
         if ($this->currencyPosition == 'after') {
@@ -70,7 +79,8 @@ class Cart extends Component {
         }
     }
 
-    public function getCount() {
+    public function getCount()
+    {
         $count = intval($this->_cart->getElements()->sum('count'));
 
         $cartEvent = new CartEvent(['cart' => $this->_cart, 'count' => $count]);
@@ -82,7 +92,8 @@ class Cart extends Component {
         return $count;
     }
 
-    public function getElements($withModel = true) {
+    public function getElements($withModel = true)
+    {
         $returnModels = [];
         foreach ($this->_cart->elements as $element) {
             if ($withModel && class_exists($element->model)) {
@@ -97,15 +108,18 @@ class Cart extends Component {
         return $returnModels; 
     }
 
-    public function getElementByModel(\pistol88\cart\interfaces\CartElement $model) {
+    public function getElementByModel(\pistol88\cart\interfaces\CartElement $model)
+    {
         return $this->_cart->getElements()->andWhere(['model' => get_class($model), 'item_id' => $model->id])->one();
     }
 
-    public function getElementById($id) {
+    public function getElementById($id)
+    {
         return $this->_cart->getElements()->andWhere(['id' => $id])->one();
     }
 
-    public function haveModelElements($modelName) {
+    public function haveModelElements($modelName)
+    {
         if ($this->_cart->getElements()->andWhere(['model' => $modelName, 'price' => 0])->one()) {
             return true;
         } else {
@@ -113,7 +127,8 @@ class Cart extends Component {
         }
     }
 
-    public function put(\pistol88\cart\interfaces\CartElement $model, $count = 1, $description = '') {
+    public function put(\pistol88\cart\interfaces\CartElement $model, $count = 1, $description = '')
+    {
         if (!$elementModel = $this->hasElement($model, $description)) {
             $elementModel = new CartElement;
             $elementModel->count = (int)$count;
@@ -142,7 +157,8 @@ class Cart extends Component {
         }
     }
     
-    public function truncate() {
+    public function truncate()
+    {
         foreach($this->_cart->elements as $element) {
             $element->delete();
         }
@@ -152,7 +168,8 @@ class Cart extends Component {
         return $this->_cart;
     }
 
-    public function hasElement(\pistol88\cart\interfaces\CartElement $model, $description = '') {
+    public function hasElement(\pistol88\cart\interfaces\CartElement $model, $description = '')
+    {
         return $this->_cart->getElements()->where(['description' => $description, 'model' => get_class($model), 'item_id' => $model->id])->one();
     }
 }
