491db9df09ebb13a15912c3f8fdab37ab90c62cb
Alexandr <alexandr.anciferov@gmail.com>
2016-09-27 16:02:41 +0300

    Update ChangeOptions.php

--- widgets/ChangeOptions.php
+++ widgets/ChangeOptions.php
@@ -1,5 +1,5 @@
 <?php
-namespace pistol88\cart\widgets; 
+namespace pistol88\cart\widgets;
 
 use yii\helpers\Url;
 use yii\helpers\Html;
@@ -9,7 +9,7 @@ class ChangeOptions extends \yii\base\Widget
 {
     const TYPE_SELECT = 'select';
     const TYPE_RADIO = 'radio';
-    
+
     public $model = NULL;
     public $type = NULL;
     public $cssClass = '';
@@ -20,17 +20,17 @@ class ChangeOptions extends \yii\base\Widget
         if ($this->type == NULL) {
             $this->type = self::TYPE_SELECT;
         }
-        
+
         parent::init();
-        
+
         \pistol88\cart\assets\WidgetAsset::register($this->getView());
-        
+
         return true;
     }
 
     public function run()
     {
-        if($this->model instanceof \pistol88\cart\interfaces\CartElement) {
+        if ($this->model instanceof \pistol88\cart\interfaces\CartElement) {
             $optionsList = $this->model->getCartOptions();
             $changerCssClass = 'pistol88-option-values-before';
             $id = $this->model->getCartId();
@@ -41,53 +41,52 @@ class ChangeOptions extends \yii\base\Widget
             $changerCssClass = 'pistol88-option-values';
         }
 
-        if(!empty($optionsList)) {
+        if (!empty($optionsList)) {
             $i = 1;
-            foreach($optionsList as $optionId => $optionData) {
-                if(!is_array($values)) {
-                    $values = [];
+            foreach ($optionsList as $optionId => $optionData) {
+                if (!is_array($optionData)) {
+                    $optionData = [];
                 }
                 
                 $cssClass = "{$changerCssClass} pistol88-cart-option{$id} ";
-                
+
                 $optionsArray = ['' => $optionData['name']];
-                foreach($optionData['variants'] as $variantId => $value) {
+                foreach ($optionData['variants'] as $variantId => $value) {
                     $optionsArray[$variantId] = $value;
                 }
-                
-                if($this->type == 'select') {
 
-                    $list = Html::dropDownList('cart_options' . $id .'-' . $i,
-                        $this->_defaultValue($option),
+                if ($this->type == 'select') {
+
+                    $list = Html::dropDownList('cart_options' . $id . '-' . $i,
+                        $this->_defaultValue($optionId),
                         $optionsArray,
                         ['data-href' => Url::toRoute(["/cart/element/update"]), 'data-filter-id' => $optionId, 'data-name' => Html::encode($optionData['name']), 'data-id' => $id, 'class' => "form-control $cssClass"]
                     );
                 } else {
                     $list = Html::tag('div', Html::tag('strong', $optionData['name']), ['class' => 'pistol88-option-heading']);
                     $list .= Html::radioList('cart_options' . $id . '-' . $i,
-                        $this->_defaultValue($option),
+                        $this->_defaultValue($optionId),
                         $optionsArray,
                         ['itemOptions' => ['data-href' => Url::toRoute(["/cart/element/update"]), 'data-filter-id' => $optionId, 'data-name' => Html::encode($optionData['name']), 'data-id' => $id, 'class' => $cssClass]]
                     );
                 }
-                
+
                 $options[] = Html::tag('div', $list, ['class' => "pistol88-option"]);
                 $i++;
             }
-        }
-        else {
+        } else {
             return null;
         }
-        
+
         return Html::tag('div', implode('', $options), ['class' => 'pistol88-change-options ' . $this->cssClass]);
     }
-    
+
     private function _defaultValue($option)
     {
-        if(isset($this->defaultValues[$option])) {
+        if (isset($this->defaultValues[$option])) {
             return $this->defaultValues[$option];
         }
-        
+
         return false;
     }
 }
